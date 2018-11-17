780f33578139cdf8f93ce8c3882ed3baddcd2ea5
Marcin Chalecki <alcaster0@gmail.com>
2018-11-17 13:11:19 +0100

    Buletproof run bash script.

--- run.sh
+++ run.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+set -e
 if [ -d ./env3 ]; then
-  echo 'Environment found, starting program.' &&
-  source env3/bin/activate &&
-  python3 source/solution.py "$@"
+  echo 'Environment found, starting program.'
 else
   echo 'Environment not found, initialising.'
-  yes | sudo apt-get install python3-venv &&
-  yes | sudo apt-get install python3-tk &&
-  mkdir env3 &&
-  python3 -m venv env3/ &&
-  source env3/bin/activate &&
-  pip install -r requirements.txt &&
-  echo 'TO START: ./run.sh --help'
+  for package in python3-venv python3-tk; do
+      if ! dpkg-query -W -f='${Status}' $package | grep "ok installed" &>/dev/null; then
+      echo "No $package, installing.
+Running command 'sudo apt install $package', require sudo.";
+      sudo apt install $package;
+      else
+        echo "$package detected"
+      fi
+  done
+  python3 -m venv env3/
 fi
+source env3/bin/activate
+echo "Installing missing python packages."
+pip install -r requirements.txt
+python3 source/solution.py "$@"
